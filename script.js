8e903244cc83beff62596c2788226f9e3a85f4a7
conecttheo <theodamata@gmail.com>
2023-11-14 16:33:18 -0300

    encrementando o jogo

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var numeroSecreto = parseInt(Math.random() * 1001);
+let tentativas = 1;
+
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+ }
+
 
 function Chutar() {
     var elementoResultado = document.getElementById('resultado');
     var chute = parseInt(document.getElementById('valor').value);
         
-   // while (chute != numeroSecreto) {
-
         if (chute == numeroSecreto) {
             elementoResultado.innerHTML = 
             'Acertou! Parabéns você é um gênio!! O número secreto é ' + numeroSecreto;
+            let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+            let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+            exibirTextoNaTela('p', mensagemTentativas);
+            document.getElementById('reiniciar').removeAttribute('disabled');
                 } 
                 else if (chute > 1000 || chute < 0  ) {
                     elementoResultado.innerHTML = 
@@ -21,10 +31,22 @@ function Chutar() {
                             else if (chute < numeroSecreto) {
                                 elementoResultado.innerHTML = 
                                 'ERROU!.. O número é MAIOR';
-                                    } 
-                                    
-                               
-    }                            
+                                    }    
+                                    tentativas++;
+                                    limparCampo();                                    
+}       
+
+function limparCampo() {
+    chute = document.getElementById('valor');
+    chute.value = '';
+}    
+        
+function reiniciarJogo() {
+    numeroSecreto = Chutar();
+    limparCampo();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+        
 
 
 
