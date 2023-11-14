64663a63c02ccc6443abfc0f1eb7cedfdf4b288d
conecttheo <theodamata@gmail.com>
2023-11-14 15:43:38 -0300

    update no script

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 var numeroSecreto = parseInt(Math.random() * 1001);
-    while (chute != numeroSecreto) {
-        var chute = prompt('Digite um numero entre 0 e 1000');
-        if(chute == numeroSecreto) {
-            alert('Acertou! Parabéns você é um gênio!!')
-        } else if (chute > numeroSecreto) {
-            alert('ERROU!.. O número é menor')
-        } else if (chute < numeroSecreto) {
-            alert('ERROU!.. O número é maior')
-        }
-    }
+
+function Chutar() {
+    var elementoResultado = document.getElementById('resultado');
+    var chute = parseInt(document.getElementById('valor').value);
+        
+   // while (chute != numeroSecreto) {
+
+        if (chute == numeroSecreto) {
+            elementoResultado.innerHTML = 
+            'Acertou! Parabéns você é um gênio!! O número secreto é ' + numeroSecreto;
+                } 
+                else if (chute > 1000 || chute < 0  ) {
+                    elementoResultado.innerHTML = 
+                    'Você deve inserir um número entre 0 e 1000';
+                    }
+                    else if (chute > numeroSecreto) {
+                        elementoResultado.innerHTML = 
+                        'ERROU!.. O número é MENOR';
+                            } 
+                            else if (chute < numeroSecreto) {
+                                elementoResultado.innerHTML = 
+                                'ERROU!.. O número é MAIOR';
+                                    } 
+                                    
+                               
+    }                            
+
+
+
+
+
+
+
+    
 
 
 
