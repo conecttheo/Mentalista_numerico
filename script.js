d274aadd3daaef7db4f5b26168ce251aba6aceb5
conecttheo <theodamata@gmail.com>
2023-11-22 10:32:08 -0300

    treinando botoes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var numeroSecreto = parseInt(Math.random() * 1001);
+var numeroSecreto = parseInt(Math.random() * 11);
 let tentativas = 1;
 
 
@@ -6,12 +6,20 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
  }
-
-
+ 
+//var botao = document.querySelector("confirmar"); 
+//    botao.addEventListener("keypress" , function(event) {
+//        if (event.key === "Enter") {
+//            event.preventDefault();
+//    
+//            Chutar();
+//        }
+//    } );
+ 
 function Chutar() {
     var elementoResultado = document.getElementById('resultado');
     var chute = parseInt(document.getElementById('valor').value);
-        
+   
         if (chute == numeroSecreto) {
             elementoResultado.innerHTML = 
             'Acertou! Parabéns você é um gênio!! O número secreto é ' + numeroSecreto;
@@ -20,21 +28,22 @@ function Chutar() {
             exibirTextoNaTela('p', mensagemTentativas);
             document.getElementById('reiniciar').removeAttribute('disabled');
                 } 
-                else if (chute > 1000 || chute < 0  ) {
+                else if (chute > 10 || chute < 0  ) {
                     elementoResultado.innerHTML = 
-                    'Você deve inserir um número entre 0 e 1000';
+                    'Você deve inserir um número entre 0 e 10';
                     }
                     else if (chute > numeroSecreto) {
                         elementoResultado.innerHTML = 
-                        'ERROU!.. O número é MENOR';
+                        'ERROU! Não é ' + chute + '. O número secreto é MENOR';
                             } 
                             else if (chute < numeroSecreto) {
                                 elementoResultado.innerHTML = 
-                                'ERROU!.. O número é MAIOR';
+                                'ERROU! Não é ' + chute + '. O número secreto é MAIOR';
                                     }    
                                     tentativas++;
                                     limparCampo();                                    
-}       
+     
+}
 
 function limparCampo() {
     chute = document.getElementById('valor');
@@ -44,6 +53,8 @@ function limparCampo() {
 function reiniciarJogo() {
     numeroSecreto = Chutar();
     limparCampo();
+    tentativas = 1;
+    exibirTextoNaTela();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
         
